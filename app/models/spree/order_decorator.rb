f31ffcc5fe99613dd998a4a327330ec3eed89d61
Sytze Loor <sytze@tweedledum.eu>
2013-11-13 10:07:23 +0100

    Altered check on agreed_to_terms once saved in the database, validate acceptance works only with '1' or '0'

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,4 +3,8 @@ Spree::Order.class_eval do
   attr_accessor :should_validate_terms
 
   validates :agreed_to_terms, acceptance: true, if: Proc.new { |order| order.send(:require_email) || order.should_validate_terms }
+
+  def agreed_to_terms
+    read_attribute(:agreed_to_terms) == true ? '1' : '0'
+  end
 end
