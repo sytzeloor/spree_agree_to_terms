4ebadb7102ff96baf29657016833c1bc2db683dd
Sytze Loor <sytze@tweedledum.eu>
2013-12-10 09:42:43 +0100

    Updated gem to work with Spree 2.1.3 and Rails 4

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,4 @@
 Spree::Order.class_eval do
-  attr_accessible :agreed_to_terms
   attr_accessor :should_validate_terms
 
   validates :agreed_to_terms, acceptance: true, if: Proc.new { |order| order.send(:require_email) || order.should_validate_terms }
